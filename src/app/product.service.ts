345e97e22435ce5ed2d9c386b3aff1716d8dcd3b
Gilbert Gotora <gilbertgotora2@gmail.com>
2019-10-23 22:27:57 +0200

    end module

---
+++ src/app/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
+
+@Injectable()
+export class ProductService {
+  private _albumUrl = "../assets/album.json";
+
+  constructor(private _http: HttpClient) { }
+
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl).
+      pipe(
+        map(response => response)
+      )
+
+
+
+  }
+
+}
