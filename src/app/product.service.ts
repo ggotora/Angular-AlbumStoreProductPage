bc29b9753e28dd4f7cf3f11418d1bacef9a08c6a
Gilbert Gotora <gilbertgotora2@gmail.com>
2019-10-24 00:24:14 +0200

    went back to HttpModule

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators'
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
   private _albumUrl = "../assets/album.json";
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: Http) { }
 
   getAlbum(id: number){
     return this._http.get(this._albumUrl).
-      pipe(
-        map(response => response)
-      )
+    map(response => response.json())
 
 
 
