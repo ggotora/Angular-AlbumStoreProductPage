0476ee39459bc9a6aa1ad4b7305e1af9e6a18366
Gilbert Gotora <gilbertgotora2@gmail.com>
2019-10-24 02:36:19 +0200

    spelling error corrrection

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs/Observable';
 export class ProductService {
   private _albumUrl = "../assets/album.json";
 
-  private _productUrl = "../assets/products.json"
+  private _productsUrl = "../assets/products.json"
 
   constructor(private _http: Http) { }
 
@@ -18,8 +18,8 @@ export class ProductService {
     return this._http.get(this._albumUrl).
     map(response => <Album>response.json())
   }
-  getProducts(){
-    return this._http.get(this._productUrl)
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl)
       .map(response => <Product[]>response.json())
   }
 
